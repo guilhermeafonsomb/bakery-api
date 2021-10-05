5c943fe53811b7ec3919afb8e6b32a27588de657
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2021-10-05 15:46:28 -0300

    criando rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { productsRoutes } from "./products.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
 router.use('/users', usersRoutes);
+router.use('/products', productsRoutes);
 
 export { router };
