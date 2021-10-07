565e4210b080f9b8584bd54a402458df34046434
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2021-10-06 22:59:05 -0300

    criando container inventory

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,6 +1,8 @@
 import { container } from "tsyringe"; 
 import { IUsersRepository, UsersRepository } from "../repositories/user/UsersRepository";
 import { IRawMaterialsRepository, RawMaterialsRepository } from "../repositories/rawMaterials/RawMaterialsRepository";
+import { IInventoryRepository, InventoryRepository } from "../repositories/inventory/InventoryRepository";
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 container.registerSingleton<IRawMaterialsRepository>('RawMaterialsRepository', RawMaterialsRepository);
+container.registerSingleton<IInventoryRepository>('InventoryRepository', InventoryRepository);
